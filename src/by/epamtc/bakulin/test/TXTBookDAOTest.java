664d83ff8ae8e22610f8416f3e85fc5870e54869
Alexey <alexey.bakulin96@gmail.com>
2021-10-28 22:52:17 +0300

    [add]: findAllBooks() tests added

--- src/by/epamtc/bakulin/test/TXTBookDAOTest.java
+++ src/by/epamtc/bakulin/test/TXTBookDAOTest.java
@@ -1,2 +1,22 @@
-package by.epamtc.bakulin.test;public class TXTBookDAOTest {
+package by.epamtc.bakulin.test;
+
+import by.epamtc.bakulin.dao.BookDAO;
+import by.epamtc.bakulin.dao.impl.TXTBookDAO;
+import by.epamtc.bakulin.io.impl.IOManagerTXT;
+import by.epamtc.bakulin.model.Book;
+import org.junit.Test;
+
+import java.util.List;
+
+public class TXTBookDAOTest {
+
+    BookDAO bookDAO = new TXTBookDAO(new IOManagerTXT());
+
+    @Test
+    public void test1() {
+        List<Book> books = bookDAO.findAllBooks();
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
 }
