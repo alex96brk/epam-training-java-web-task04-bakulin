cc01d8d96f44f84eeed6534eb0fb505dfab45d5f
Alexey <alexey.bakulin96@gmail.com>
2021-10-30 18:22:47 +0300

    [add]: txtDAO for entities implementation added

--- src/by/epamtc/bakulin/test/TXTBookDAOTest.java
+++ src/by/epamtc/bakulin/test/TXTBookDAOTest.java
@@ -1,8 +1,9 @@
 package by.epamtc.bakulin.test;
 
-import by.epamtc.bakulin.dao.BookDAO;
+import by.epamtc.bakulin.dao.DAO;
+import by.epamtc.bakulin.dao.exception.DAOException;
 import by.epamtc.bakulin.dao.impl.TXTBookDAO;
-import by.epamtc.bakulin.io.impl.IOManagerTXT;
+import by.epamtc.bakulin.io.impl.IOConnectorTXT;
 import by.epamtc.bakulin.model.Book;
 import org.junit.Test;
 
@@ -10,52 +11,32 @@ import java.util.List;
 
 public class TXTBookDAOTest {
 
-    BookDAO dao = new TXTBookDAO(new IOManagerTXT());
+    DAO<Book> dao = new TXTBookDAO(new IOConnectorTXT());
 
     @Test
-    public void test1() {
-        List<Book> books = dao.findAllBooks();
+    public void test1() throws DAOException {
+        List<Book> books = dao.findAll();
         for (Book book : books) {
             System.out.println(book);
         }
     }
 
     @Test
-    public void test2() {
-        Book book = dao.createBook("Mist", "Steven King", "Psychological Horror");
-        System.out.println("New book: " + book);
+    public void test2() throws DAOException {
+        Book book = new Book("Mist", "Steven King", "Horror Story");
+        dao.add(book);
     }
 
     @Test
-    public void test21() {
-        Long searchBookByBookId = 5281449453290762240L;
-        Book book = dao.findBookByBookId(searchBookByBookId);
-        System.out.println("Search result: " + book);
+    public void test3() throws DAOException {
+        Book book = dao.findById(7634399147678983168L);
+        book.setBookName("Quiet Don");
+        dao.update(book);
     }
 
     @Test
-    public void test3() {
-        String searchBookByBookName = "Altered Carbon";
-        Book book = dao.findBookByBookName(searchBookByBookName);
-        System.out.println("Search result: " + book);
+    public void test4() throws DAOException {
+        dao.delete(3269654306464640000L);
     }
 
-    @Test
-    public void test4() {
-        String searchBookByAuthor = "Frank Herbert";
-        Book book = dao.findBookByBookName(searchBookByAuthor);
-        System.out.println("Search result: " + book);
-    }
-
-    @Test
-    public void test5() {
-        dao.updateBook(8944158836706102272L, "Code Complete", "Steve McConnell", "Computer Science");
-        test1();
-    }
-
-    @Test
-    public void test6() {
-        String id = "1783575304007059456";
-        dao.deleteBook(Long.parseLong(id));
-    }
 }
--- src/by/epamtc/bakulin/test/TXTUserDAOTest.java
+++ src/by/epamtc/bakulin/test/TXTUserDAOTest.java
@@ -1,8 +1,9 @@
 package by.epamtc.bakulin.test;
 
-import by.epamtc.bakulin.dao.UserDAO;
+import by.epamtc.bakulin.dao.DAO;
+import by.epamtc.bakulin.dao.exception.DAOException;
 import by.epamtc.bakulin.dao.impl.TXTUserDAO;
-import by.epamtc.bakulin.io.impl.IOManagerTXT;
+import by.epamtc.bakulin.io.impl.IOConnectorTXT;
 import by.epamtc.bakulin.model.Role;
 import by.epamtc.bakulin.model.User;
 import org.junit.Test;
@@ -10,45 +11,34 @@ import org.junit.Test;
 import java.util.List;
 
 public class TXTUserDAOTest {
-    UserDAO dao = new TXTUserDAO(new IOManagerTXT());
+
+    DAO<User> dao = new TXTUserDAO(new IOConnectorTXT());
 
     @Test
-    public void test1() {
-        List<User> users = dao.findAllUsers();
+    public void test1() throws DAOException {
+        List<User> users = dao.findAll();
         for (User user : users) {
             System.out.println(user);
         }
     }
 
     @Test
-    public void test2() {
-        User newUser =  dao.createUser("janeFrosty", "Jane", "Frost", "124josidjo");
-        System.out.println("New User: " + newUser);
-        System.out.println("All users: ");
-        test1();
+    public void test2() throws DAOException {
+        User user = new User("hover14", "Alex", "Hover", "ahahfoa14");
+        dao.add(user);
     }
 
     @Test
-    public void test3() {
-        String searchName = "janeFrosty";
-        User searchUser = dao.findUserByUserName(searchName);
-        System.out.println("Result: " + searchUser);
+    public void test3() throws DAOException {
+        User user = dao.findById(179857332653907968L);
+        user.setUserName("Alexander");
+        user.setUserRole(Role.ADMIN);
+        dao.update(user);
     }
 
     @Test
-    public void test4() {
-        dao.updateUser(4045569050689157120L, "andrey456holod", "Andrey", "Holodnov", "sjhkjsg2345", Role.USER);
-        List<User> users = dao.findAllUsers();
-        for (User user : users) {
-            System.out.println(user);
-        }
+    public void test4() throws DAOException {
+        dao.delete(179857332653907968L);
     }
-
-    @Test
-    public void test5() {
-        dao.deleteUser(4045569050689157120L);
-    }
-
-
 
 }
