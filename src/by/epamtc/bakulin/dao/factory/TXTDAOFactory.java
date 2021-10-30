c9a90ddef781021692c909955d4a933300d6539f
Alexey <alexey.bakulin96@gmail.com>
2021-10-30 22:00:09 +0300

    [add]: TXTDAO implementation

--- src/by/epamtc/bakulin/dao/factory/TXTDAOFactory.java
+++ src/by/epamtc/bakulin/dao/factory/TXTDAOFactory.java
@@ -1,11 +1,28 @@
 package by.epamtc.bakulin.dao.factory;
 
+import by.epamtc.bakulin.dao.BookDAO;
 import by.epamtc.bakulin.dao.UserDAO;
+import by.epamtc.bakulin.dao.impl.TXTBookDAO;
 import by.epamtc.bakulin.dao.impl.TXTUserDAO;
+import by.epamtc.bakulin.io.IOConnector;
+import by.epamtc.bakulin.io.impl.IOConnectorTXT;
 
-public class UserDAOFactory implements DAOFactory<UserDAO> {
-    @Override
-    public UserDAO getDAO() {
-        return new TXTUserDAO();
+public class TXTDAOFactory {
+
+    private static final TXTDAOFactory INSTANCE = new TXTDAOFactory();
+    private static final IOConnector IO_CONNECTOR = new IOConnectorTXT();
+
+    private TXTDAOFactory() {}
+
+    public static TXTDAOFactory getInstance() {
+        return INSTANCE;
+    }
+
+    public UserDAO getUserDAO() {
+        return new TXTUserDAO(IO_CONNECTOR);
+    }
+
+    public BookDAO getBookDAO() {
+        return new TXTBookDAO(IO_CONNECTOR);
     }
 }
