e19b8935f1f853280c85bfe60b476c30cdd3a45f
Alexey <alexey.bakulin96@gmail.com>
2021-10-31 15:16:24 +0300

    [edit]: add overloaded concstructor E(e) to implementation

--- src/by/epamtc/bakulin/dao/exception/DAOException.java
+++ src/by/epamtc/bakulin/dao/exception/DAOException.java
@@ -5,4 +5,8 @@ public class DAOException extends Exception {
     public DAOException(String message) {
         super(message);
     }
+
+    public DAOException(Exception e) {
+        super(e);
+    }
 }
