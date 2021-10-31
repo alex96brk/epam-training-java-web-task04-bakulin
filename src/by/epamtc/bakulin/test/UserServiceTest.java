b1482590ed0f04dac9bc63396f655610a7aa544f
Alexey <alexey.bakulin96@gmail.com>
2021-10-31 19:17:02 +0300

    [edit]: UserServiceTest impl added

--- src/by/epamtc/bakulin/test/UserServiceTest.java
+++ src/by/epamtc/bakulin/test/UserServiceTest.java
@@ -1,2 +1,22 @@
-package by.epamtc.bakulin.test;public class UserServiceTest {
+package by.epamtc.bakulin.test;
+
+import by.epamtc.bakulin.dao.factory.TXTDAOFactory;
+import by.epamtc.bakulin.entity.User;
+import by.epamtc.bakulin.service.UserService;
+import by.epamtc.bakulin.service.exception.ServiceException;
+import by.epamtc.bakulin.service.impl.UserServiceImpl;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserServiceTest {
+    UserService userService = new UserServiceImpl(TXTDAOFactory.getInstance().getUserDAO());
+
+    @Test
+    public void test1() throws ServiceException {
+        List<User> users = userService.findAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
 }
