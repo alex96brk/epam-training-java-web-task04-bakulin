ba92a3c3c98da9f56c1b7423b975e959eacf2488
Alexey <alexey.bakulin96@gmail.com>
2021-10-31 13:50:01 +0300

    [add]: UserServiceImpl wrapper added

---
+++ src/by/epamtc/bakulin/service/impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package by.epamtc.bakulin.service.impl;
+
+import by.epamtc.bakulin.entity.User;
+import by.epamtc.bakulin.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public User findUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User findUserByName(String userName) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
+}
