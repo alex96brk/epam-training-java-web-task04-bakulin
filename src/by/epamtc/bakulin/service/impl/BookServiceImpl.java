76e5cf5a838245039c900ed3b8e47f198f6f24d7
Alexey <alexey.bakulin96@gmail.com>
2021-10-31 15:17:13 +0300

    [add]: basic service impl added

--- src/by/epamtc/bakulin/service/impl/BookServiceImpl.java
+++ src/by/epamtc/bakulin/service/impl/BookServiceImpl.java
@@ -1,38 +1,78 @@
 package by.epamtc.bakulin.service.impl;
 
+import by.epamtc.bakulin.dao.BookDAO;
+import by.epamtc.bakulin.dao.exception.DAOException;
 import by.epamtc.bakulin.entity.Book;
 import by.epamtc.bakulin.service.BookService;
+import by.epamtc.bakulin.service.exception.ServiceException;
 
 import java.util.List;
 
 public class BookServiceImpl implements BookService {
-    @Override
-    public void addBook(Book book) {
 
+    private BookDAO bookDAO;
+
+    public void setBookDAO(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
     }
 
     @Override
-    public Book findBookById(Long id) {
-        return null;
+    public void addBook(Book book) throws ServiceException {
+        try {
+            bookDAO.add(book);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
     }
 
     @Override
-    public Book findBookByName(String bookName) {
-        return null;
+    public Book findBookById(Long id) throws ServiceException {
+        Book book = null;
+        try {
+            bookDAO.findById(id);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+        return book;
     }
 
     @Override
-    public List<Book> findAllBooks() {
-        return null;
+    public Book findBookByAuthor(String bookAuthor) throws ServiceException {
+        Book book = null;
+        try {
+            bookDAO.findByAuthor(bookAuthor);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+        return book;
     }
 
     @Override
-    public void updateBook(Book book) {
-
+    public List<Book> findAllBooks() throws ServiceException {
+        List<Book> books = null;
+        try {
+            books = bookDAO.findAll();
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+        return books;
     }
 
     @Override
-    public void deleteBook(Long id) {
+    public void updateBook(Book book) throws ServiceException {
+        try {
+            bookDAO.update(book);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+    }
 
+    @Override
+    public void deleteBook(Long id) throws ServiceException {
+        try {
+            bookDAO.delete(id);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
     }
 }
--- src/by/epamtc/bakulin/service/impl/UserServiceImpl.java
+++ src/by/epamtc/bakulin/service/impl/UserServiceImpl.java
@@ -1,38 +1,78 @@
 package by.epamtc.bakulin.service.impl;
 
+import by.epamtc.bakulin.dao.UserDAO;
+import by.epamtc.bakulin.dao.exception.DAOException;
 import by.epamtc.bakulin.entity.User;
 import by.epamtc.bakulin.service.UserService;
+import by.epamtc.bakulin.service.exception.ServiceException;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    @Override
-    public void addUser(User user) {
 
+    private UserDAO userDAO;
+
+    public void setUserDAO(UserDAO userDAO) {
+        this.userDAO = userDAO;
     }
 
     @Override
-    public User findUserById(Long id) {
-        return null;
+    public void addUser(User user) throws ServiceException {
+        try {
+            userDAO.add(user);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
     }
 
     @Override
-    public User findUserByName(String userName) {
-        return null;
+    public User findUserById(Long id) throws ServiceException {
+        User user = null;
+        try {
+            user = userDAO.findById(id);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+        return user;
     }
 
     @Override
-    public List<User> findAllUsers() {
-        return null;
+    public User findUserByName(String userName) throws ServiceException {
+        User user = null;
+        try {
+            user = userDAO.findByName(userName);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+        return user;
     }
 
     @Override
-    public void updateUser(User user) {
-
+    public List<User> findAllUsers() throws ServiceException {
+        List<User> users = null;
+        try {
+            userDAO.findAll();
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+        return users;
     }
 
     @Override
-    public void deleteUser(Long id) {
+    public void updateUser(User user) throws ServiceException{
+        try {
+            userDAO.update(user);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+    }
 
+    @Override
+    public void deleteUser(Long id) throws ServiceException {
+        try {
+            userDAO.delete(id);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
     }
 }
