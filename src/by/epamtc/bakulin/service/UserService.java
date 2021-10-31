3a646cdbfc0a3c391740388d542b4b430e5c9540
Alexey <alexey.bakulin96@gmail.com>
2021-10-31 13:40:22 +0300

    [add]: UserService interface added

--- src/by/epamtc/bakulin/service/UserService.java
+++ src/by/epamtc/bakulin/service/UserService.java
@@ -1,2 +1,22 @@
-package by.epamtc.bakulin.service;public class UserService {
+package by.epamtc.bakulin.service;
+
+import by.epamtc.bakulin.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    void addUser(User user);
+
+    User findUserById(Long id);
+
+    User findUserByName(String userName);
+
+    List<User> findAllUsers();
+
+    void updateUser(User user);
+
+    void deleteUser(Long id);
+
+
 }
