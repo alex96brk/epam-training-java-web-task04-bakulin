65aba060f8ecfbcfadd1aaa8e12146d86fd30b0b
Alexey <alexey.bakulin96@gmail.com>
2021-10-31 15:12:32 +0300

    [edit]: add throws DAOException to method signature

--- src/by/epamtc/bakulin/dao/BookDAO.java
+++ src/by/epamtc/bakulin/dao/BookDAO.java
@@ -1,8 +1,9 @@
 package by.epamtc.bakulin.dao;
 
+import by.epamtc.bakulin.dao.exception.DAOException;
 import by.epamtc.bakulin.entity.Book;
 
 public interface BookDAO extends DAO<Book> {
 
-    Book findByAuthor(String author);
+    Book findByAuthor(String author) throws DAOException;
 }
--- src/by/epamtc/bakulin/dao/UserDAO.java
+++ src/by/epamtc/bakulin/dao/UserDAO.java
@@ -1,8 +1,9 @@
 package by.epamtc.bakulin.dao;
 
+import by.epamtc.bakulin.dao.exception.DAOException;
 import by.epamtc.bakulin.entity.User;
 
 public interface UserDAO extends DAO<User> {
 
-    User findByName(String userName);
+    User findByName(String userName) throws DAOException;
 }
