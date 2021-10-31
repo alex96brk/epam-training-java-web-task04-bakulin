2af34241f3c920eacb50a4a63bab3e2d311b84b3
Alexey <alexey.bakulin96@gmail.com>
2021-10-31 13:48:46 +0300

    [add]: BookServiceImpl wrapper added

---
+++ src/by/epamtc/bakulin/service/impl/BookServiceImpl.java
@@ -0,0 +1,38 @@
+package by.epamtc.bakulin.service.impl;
+
+import by.epamtc.bakulin.entity.Book;
+import by.epamtc.bakulin.service.BookService;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+    @Override
+    public void addBook(Book book) {
+
+    }
+
+    @Override
+    public Book findBookById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Book findBookByName(String bookName) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAllBooks() {
+        return null;
+    }
+
+    @Override
+    public void updateBook(Book book) {
+
+    }
+
+    @Override
+    public void deleteBook(Long id) {
+
+    }
+}
