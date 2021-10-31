ffb852a717ba317c92ba0d269fc515f4d846c66f
Alexey <alexey.bakulin96@gmail.com>
2021-10-31 19:16:37 +0300

    [edit]: BookServiceTest impl added

--- src/by/epamtc/bakulin/test/BookServiceTest.java
+++ src/by/epamtc/bakulin/test/BookServiceTest.java
@@ -1,2 +1,23 @@
-package by.epamtc.bakulin.test;public class BookServiceTest {
+package by.epamtc.bakulin.test;
+
+import by.epamtc.bakulin.dao.factory.TXTDAOFactory;
+import by.epamtc.bakulin.entity.Book;
+import by.epamtc.bakulin.service.BookService;
+import by.epamtc.bakulin.service.exception.ServiceException;
+import by.epamtc.bakulin.service.impl.BookServiceImpl;
+import org.junit.Test;
+
+import java.util.List;
+
+public class BookServiceTest {
+    BookService bookService = new BookServiceImpl(TXTDAOFactory.getInstance().getBookDAO());
+
+    @Test
+    public void test1() throws ServiceException {
+        List<Book> books = bookService.findAllBooks();
+        for (Book book : books) {
+            System.out.println(book);
+        }
+
+    }
 }
