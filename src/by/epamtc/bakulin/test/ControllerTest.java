09af70e974b44816a058bc853e3aa3439e2e7c6f
Alexey <alexey.bakulin96@gmail.com>
2021-11-02 00:12:49 +0300

    [add]: controller test

--- src/by/epamtc/bakulin/test/ControllerTest.java
+++ src/by/epamtc/bakulin/test/ControllerTest.java
@@ -1,2 +1,16 @@
-package by.epamtc.bakulin.test;public class ControllerTest {
+package by.epamtc.bakulin.test;
+
+import by.epamtc.bakulin.controller.Controller;
+import org.junit.Test;
+
+public class ControllerTest {
+
+    private Controller controller = new Controller();
+
+    @Test
+    public void test1() {
+        String cmdRequest = "book_find_all";
+        String result = controller.runCommand(cmdRequest);
+        System.out.println(result);
+    }
 }
