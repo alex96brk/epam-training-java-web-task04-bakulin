8dbc3dbdc74aa2e91080b659e3754b5c14b501af
Alexey <alexey.bakulin96@gmail.com>
2021-11-01 18:02:14 +0300

    [add]: CommandSequenceTest added (books)

--- src/by/epamtc/bakulin/test/CommandSequenceTest.java
+++ src/by/epamtc/bakulin/test/CommandSequenceTest.java
@@ -1,2 +1,48 @@
-package by.epamtc.bakulin.test;public class CommandSequenceTest {
+package by.epamtc.bakulin.test;
+
+import by.epamtc.bakulin.controller.CommandSequence;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+public class CommandSequenceTest {
+
+    private CommandSequence commandSequence = new CommandSequence();
+
+    @Test
+    public void test1() {
+        String cmdRequest = "book_find_all";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+
+    @Test
+    public void test2() {
+        String cmdRequest = "book_find_by_id $1083762518538963968";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+
+    @Test
+    public void test3() {
+        String cmdRequest = "book_add $Dune $Frank Herbert $Fantastic Novel";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+
+    @Test
+    public void test4() {
+        String cmdRequest = "book_delete $3765047529279459328";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+
+    @Test
+    public void test5() {
+        String cmdRequest = "book_find_by_author $Frank Herbert";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+
+    @Test
+    public void test6() {
+        String cmdRequest = "book_update $8944158836706102272 $Code Complete $Steve McConnel $Computer Science";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+
 }
