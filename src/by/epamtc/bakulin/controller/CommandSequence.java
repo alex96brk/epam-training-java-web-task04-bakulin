1d80a12f50bc1f376f1ec4b84c615282480e1b2d
Alexey <alexey.bakulin96@gmail.com>
2021-11-01 17:59:05 +0300

    [add]: CommandSequence raw impl added

--- src/by/epamtc/bakulin/controller/CommandSequence.java
+++ src/by/epamtc/bakulin/controller/CommandSequence.java
@@ -1,11 +1,53 @@
 package by.epamtc.bakulin.controller;
 
 import by.epamtc.bakulin.controller.command.Command;
-import by.epamtc.bakulin.controller.command.CommandSequence;
+import by.epamtc.bakulin.controller.command.CmdId;
+import by.epamtc.bakulin.controller.command.impl.app.BadRequestCommand;
+import by.epamtc.bakulin.controller.command.impl.book.*;
+import by.epamtc.bakulin.controller.command.impl.user.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class CommandRepository {
-    private Map<CommandSequence, Command> repository = new HashMap<>();
+import static by.epamtc.bakulin.controller.command.CmdId.*;
+
+public class CommandSequence {
+    private Map<CmdId, Command> sequence = new HashMap<>();
+    {
+        sequence.put(USER_ADD, new UserAddCommand());
+        sequence.put(USER_FIND_BY_ID, new UserFindByIdCommand());
+        sequence.put(USER_FIND_BY_UNAME, new UserFindByNameCommand());
+        sequence.put(USER_FIND_ALL, new UserFindAllCommand());
+        sequence.put(USER_UPDATE, new UserUpdateCommand());
+        sequence.put(USER_DELETE, new UserDeleteCommand());
+
+        sequence.put(BOOK_ADD, new BookAddCommand());
+        sequence.put(BOOK_FIND_BY_ID, new BookFindByIdCommand());
+        sequence.put(BOOK_FIND_BY_AUTHOR, new BookFindByAuthorCommand());
+        sequence.put(BOOK_FIND_ALL, new BookFindAllCommand());
+        sequence.put(BOOK_UPDATE, new BookUpdateCommand());
+        sequence.put(BOOK_DELETE, new BookDeleteCommand());
+
+        sequence.put(BAD_REQUEST, new BadRequestCommand());
+    }
+
+    public CommandSequence() {}
+
+    public Command getCommand(String cmdRequest) {
+        String[] target = cmdRequest.split(" \\$");
+        Command command = null;
+
+        if (cmdRequest != null) {
+            try {
+                CmdId cmdId = CmdId.valueOf(target[0].toUpperCase());
+                command = sequence.get(cmdId);
+            } catch (IllegalArgumentException exception) {
+                command = sequence.get(BAD_REQUEST);
+            }
+        }
+
+        return command;
+    }
+
+
 }
