051c959be76246889ad5bc4291bddeec35cdb4d5
Alexey <alexey.bakulin96@gmail.com>
2021-11-01 18:46:18 +0300

    [add]: tests for UserCommands added

--- src/by/epamtc/bakulin/test/CommandSequenceTest.java
+++ src/by/epamtc/bakulin/test/CommandSequenceTest.java
@@ -3,46 +3,77 @@ package by.epamtc.bakulin.test;
 import by.epamtc.bakulin.controller.CommandSequence;
 import org.junit.Test;
 
-import java.util.Scanner;
-
 public class CommandSequenceTest {
 
     private CommandSequence commandSequence = new CommandSequence();
 
     @Test
-    public void test1() {
+    public void testBookCommand1() {
         String cmdRequest = "book_find_all";
         System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
     }
 
     @Test
-    public void test2() {
+    public void testBookCommand2() {
         String cmdRequest = "book_find_by_id $1083762518538963968";
         System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
     }
 
     @Test
-    public void test3() {
+    public void testBookCommand3() {
         String cmdRequest = "book_add $Dune $Frank Herbert $Fantastic Novel";
         System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
     }
 
     @Test
-    public void test4() {
+    public void testBookCommand4() {
         String cmdRequest = "book_delete $3765047529279459328";
         System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
     }
 
     @Test
-    public void test5() {
+    public void testBookCommand5() {
         String cmdRequest = "book_find_by_author $Frank Herbert";
         System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
     }
 
     @Test
-    public void test6() {
+    public void testBookCommand6() {
         String cmdRequest = "book_update $8944158836706102272 $Code Complete $Steve McConnel $Computer Science";
         System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
     }
+
+    @Test
+    public void testUserCommand1() {
+        String cmdRequest = "user_find_all";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+
+    @Test
+    public void testUserCommand2() {
+        String cmdRequest = "user_find_by_id $3107024621590589440";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+    @Test
+    public void testUserCommand3() {
+        String cmdRequest = "user_add $hromovSanches $Alexander $Hromov $sogha123354";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+    @Test
+    public void testUserCommand4() {
+        String cmdRequest = "user_delete $1875660703570902016";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+    @Test
+    public void testUserCommand5() {
+        String cmdRequest = "user_find_by_uname $tamaVoron";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+    @Test
+    public void testUserCommand6() {
+        String cmdRequest = "user_update $9043868949340740608 $anderHolod $Holodnov $Andrew  $sglhs09 $admin";
+        System.out.println(commandSequence.getCommand(cmdRequest).execute(cmdRequest));
+    }
+    //User{ userId = 9043868949340740608, userName = 'anderHolod', lastName = 'Holodny', firstName = 'Andrey', userRole = 'USER', password = 'sglhs09'}
 
 }
