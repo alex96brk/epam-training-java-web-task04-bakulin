4add4b34a858ed6fcab4613c59a508e1d874742b
Alexey <alexey.bakulin96@gmail.com>
2021-11-01 23:58:42 +0300

    [add]: controller raw impl added

--- src/by/epamtc/bakulin/controller/Controller.java
+++ src/by/epamtc/bakulin/controller/Controller.java
@@ -1,4 +1,20 @@
 package by.epamtc.bakulin.controller;
 
+import by.epamtc.bakulin.controller.command.Command;
+
 public class Controller {
+
+    private static final CommandSequence commandSequence = new CommandSequence();
+
+    public static CommandSequence getCommandSequence() {
+        return commandSequence;
+    }
+
+    public String runCommand(String cmdRequest) {
+
+        Command command = commandSequence.getCommand(cmdRequest);
+        return command.execute();
+    }
+
+
 }
