cfd187c1bd92325e686f728345df92cd79f319d2
Alexey <alexey.bakulin96@gmail.com>
2021-11-01 18:01:10 +0300

    [add]: bad request command wrapper added

--- src/by/epamtc/bakulin/controller/command/impl/app/BadRequestCommand.java
+++ src/by/epamtc/bakulin/controller/command/impl/app/BadRequestCommand.java
@@ -1,2 +1,13 @@
-package by.epamtc.bakulin.controller.command.impl.app;public class BadRequestCommand {
+package by.epamtc.bakulin.controller.command.impl.app;
+
+import by.epamtc.bakulin.controller.command.Command;
+
+public class BadRequestCommand implements Command {
+
+    private static final String cmdResponse = "Bad Request";
+
+    @Override
+    public String execute(String cmdRequest) {
+        return cmdResponse;
+    }
 }
