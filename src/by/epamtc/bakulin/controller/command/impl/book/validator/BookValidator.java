14723c63ee044613adab85f2c7f8f55d7956b6e8
Alexey <alexey.bakulin96@gmail.com>
2021-11-02 13:54:55 +0300

    [add]: book validator

--- src/by/epamtc/bakulin/controller/command/impl/book/validator/BookValidator.java
+++ src/by/epamtc/bakulin/controller/command/impl/book/validator/BookValidator.java
@@ -1,2 +1,42 @@
-package by.epamtc.bakulin.controller.command.impl.book.validator;public class BookValidator {
+package by.epamtc.bakulin.controller.command.impl.book.validator;
+
+import by.epamtc.bakulin.service.exception.ServiceException;
+
+public class BookValidator {
+
+    public static void validateBookProperties(Integer id, String bookName, String bookAuthor, String bookGenre) throws ServiceException {
+        validateId(id);
+        validateBookProperties(bookName, bookAuthor, bookGenre);
+    }
+
+    public static void validateBookProperties(String bookName, String bookAuthor, String bookGenre) throws ServiceException {
+        validateBookName(bookName);
+        validateAuthor(bookAuthor);
+        validateBookGenre(bookGenre);
+    }
+
+    public static void validateId(Integer id) throws ServiceException {
+        if (id == null || id < 0) {
+            throw new ServiceException("Id can not be null or less then 0");
+        }
+    }
+
+    public static void validateBookName(String bookName) throws ServiceException {
+        if (bookName == null) {
+            throw new ServiceException("Book Name can not be null");
+        }
+    }
+
+    public static void validateBookGenre(String bookGenre) throws ServiceException {
+        if (bookGenre == null) {
+            throw new ServiceException("Book Genre can not be null");
+        }
+    }
+
+    public static void validateAuthor(String bookAuthor) throws ServiceException {
+        if (bookAuthor == null) {
+            throw new ServiceException("Author Name can not be null");
+        }
+    }
+
 }
