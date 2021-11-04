b51cff0718055a1bd3e46897e31f7b7c5e5e491d
Alexey <alexey.bakulin96@gmail.com>
2021-11-04 21:48:09 +0300

    [add]: raw runner implemenation added

--- src/by/epamtc/bakulin/runner/Runner.java
+++ src/by/epamtc/bakulin/runner/Runner.java
@@ -15,16 +15,39 @@ public class Runner {
 
     public static void main(String[] args) {
 
+        System.out.println("Library APP started.\nPlease enter your Username and Password:");
+
         UserSession userSession = new UserSession();
         String userName = sc.nextLine();
         String password = sc.nextLine();
-        System.out.println("uName: " + userName + " password: " + password);
 
         try {
             userSession.authenticate(userName, password);
         } catch (ServiceException e) {
             System.out.println("Incorrect credentials");
         }
+
+        String instructure = "Enter next cmd request:\n" +
+
+                "User requests:\n" +
+                "\tuser_find_all - find all users in the library\n" +
+                "\tuser_find_by_id $id - to find user by id\n" +
+                "\tuser_find_by_uname $userName - to find user by username\n" +
+
+                "Book requests:\n" +
+                "\tbook_find_all - find all books in the library\n" +
+                "\tbook_find_by_id $id - to find book by id\n" +
+                "\tbook_find_by_author $bookAuthor - to find book by author\n" +
+
+                "ADMIN requests:\n" +
+                "\tbook_add $bookName $bookAuthor $bookGenre - to add book into the library(ADMIN)\n" +
+                "\tbook_update $id $bookName $bookAuthor $bookGenre - to update book into the library(ADMIN)\n" +
+                "\tbook_delete $id - to delete book from the library(ADMIN)\n" +
+                "\tuser_add $userName $firstName $lastName $password - to add book into the library(ADMIN))\n" +
+                "\tuser_update $id $userName $firstName $lastName $password $role - to add book into the library(ADMIN)\n" +
+                "\tuser_delete $id - to delete user from the library(ADMIN)\n" +
+                "";
+        System.out.println(instructure);
 
         if(userSession.isSessionActive()) {
             String request = null;
